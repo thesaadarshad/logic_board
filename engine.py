618742c1f4fbf0ebd1a3d0c778140b2dc5ccdc0c
Saad Arshad <saad@Saads-MacBook-Air.local>
2021-12-20 03:53:48 +0500

    guardrails

--- engine.py
+++ engine.py
@@ -6,8 +6,6 @@ import os
 base_stages = [0.1,0.2,0.4,0.8,1.7,3.6,7.5,15.7]
 fixed_call_percentages = [0.336,0.669,1.347,2.75,5.965,13.429,32.318,99.956]
 
-available_splitters = [':','：']
-
 signal_fileName = 'trading_signal.txt'
 
 
@@ -18,9 +16,9 @@ class Engine():
 
     def __init__(self):
         load_dotenv()
+        self.original_time = 0
 
     def getInvestmentChart(self, investment):
-
         chart = []
         accumulated = 0
         for x in range(8):
@@ -32,52 +30,63 @@ class Engine():
             chart.append(stage)
         return chart
 
-    def parse_text(self):
+    def parse_text(self, message):
         parsed_message = {}
-        with open(signal_fileName) as file:
-            for line in file:
-                line = line.strip()
-                if len(line) > 1:
-
-                    if ":" in line:
-                        (key, value) = line.split(":")
-                        parsed_message[key] = value.replace(" USDT", "").strip()
-
-                    if "：" in line:
-                        (key, value) = line.split("：")
-                        parsed_message['Select'] = value.strip()
-
+        for line in message:
+            line = line.strip()
+            if len(line) > 1:
+                if ":" in line:
+                    (key, value) = line.split(":")
+                    parsed_message[key] = value.replace(" USDT", "").strip()
+                if "：" in line:
+                    (key, value) = line.split("：")
+                    parsed_message['Select'] = value.strip()
         return parsed_message
 
+    def CheckForNewMessages(self):
+        if os.path.getmtime(signal_fileName) > self.original_time:
+            self.original_time = os.path.getmtime(signal_fileName)
+            return True
+        else:
+            return False
+
+    def get_signal_message(self):
+        file = open(signal_fileName, 'r')
+        message = file.readlines()
+        return message
 
     def run(self):
-        print("--Start Trade--")
 
-        print(f"Username: {getEnvValue('HFC_USER')}")
         investment = int(getEnvValue('HFC_INVESTMENT'))
-        print(f"Investment: {investment}")
         investmentChart = self.getInvestmentChart(investment)
+
+
+        print("--Start Trade--")
+        print(f"Username: {getEnvValue('HFC_USER')}")
+        print(f"Investment: {investment}")
         print("Investment Chart:")
         print(investmentChart)
 
-        print('Waiting for Signals')
-        originalTime = 0
-
+        print('Starting Engine')
+        print("###############")
         while (True):
-            if (os.path.getmtime(signal_fileName) > originalTime):
-                print("Reading Messages")
-                originalTime = os.path.getmtime(signal_fileName)
-                parsed_message = self.parse_text()
+            if (self.CheckForNewMessages()):
+                print("New Message Found")
+
+                signal_message = self.get_signal_message()
+
+                if len(signal_message) < 1:
+                    exit('no input message found')
+
+                parsed_message = self.parse_text(signal_message)
 
                 if float(parsed_message['Amount']) in base_stages:
                     found_stage_number = base_stages.index(float(parsed_message['Amount']))
-                    print("##")
-                    print(f"Stage {found_stage_number+1} Call Found.")
-                    print(f"Call: {parsed_message['Select']}")
-                    print(f"Target Price: {investmentChart[found_stage_number]['buy']}")
-                    print("##")
+                    print(f"-stage {found_stage_number+1} Call Found.")
+                    print(f"-call: {parsed_message['Select']}")
+                    print(f"-target price: {investmentChart[found_stage_number]['buy']}")
             else:
-                print('No New Messages')
+                print('No New Messages. Refreshing...')
             time.sleep(5)
 
 
